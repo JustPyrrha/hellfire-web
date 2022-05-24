d0e49b79ca878af2a70d7a9a6a271d8f60ed1ccd
Pyrrha <pyrrha.social@gmail.com>
2022-05-24 21:57:46 +1000

    fix: remove @babel/plugin-transform-react-jsx plugin from vite build

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    babel: {
-      plugins: [
-        ["@babel/plugin-transform-react-jsx", { runtime: "automatic" }],
-      ]
-    },
-  })],
+  // plugins: [react({
+  //   babel: {
+  //     plugins: [
+  //       ["@babel/plugin-transform-react-jsx", { runtime: "automatic" }],
+  //     ]
+  //   },
+  // })],
   mode: "development",
   build: {
     minify: false,
